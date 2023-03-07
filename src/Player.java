4239943a50d95f1af5ea58b9ddafe835dce062bd
Jude Lawler <lawlerj@lawrence.edu>
2023-03-07 10:07:48 -0600

    Edited heal method to limit usage

--- src/Player.java
+++ src/Player.java
@@ -1,17 +1,20 @@
 public class Player {
     private int hitPoints;
     private String name;
+    private int numberHeals;    // implemented to limit heals available to player
 
 
 
     public Player() {           // base Player stats w/ default name
         this.hitPoints = 20;
         this.name = "Player";
+        this.numberHeals = 4;
         
     }
     public Player(String nameInput) {   // still sets HP to 20, inputs new name String
         this.hitPoints = 20;
         this.name = nameInput;
+        this.numberHeals = 4;
     }
 
     public int getHP() {
@@ -23,12 +26,19 @@ public class Player {
     }
 
     public void heal() {                // heal method increases HP by 3 if HP is 17 or lower - increases by 1 if 18 or 19, and prints statement if at 20
-        if (this.hitPoints < 18) {
-            this.hitPoints = this.hitPoints + 3;
-        } else if (this.hitPoints < 20) {
-            this.hitPoints = this.hitPoints + 1;
+        if (this.numberHeals > 0) {
+            if (this.hitPoints < 18) {
+                this.hitPoints = this.hitPoints + 3;
+                System.out.println("You heal yourself!");
+            } else if (this.hitPoints < 20) {
+                this.hitPoints = this.hitPoints + 1;
+                System.out.println("You heal yourself!");
+            } else {
+                System.out.println("You heal, but you are already at max HP!\n");
+            }
+            this.numberHeals--;     // using heal method reduces player's numberHeals by 1
         } else {
-            System.out.println("You are already at max HP!\n");
+            System.out.println("You are out of heals!");    // method does not increase HP and instead prints this if numberHeals is 0
         }
     }
 
